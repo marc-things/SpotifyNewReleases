a7e99c4718f0c5c8638fa3fad63d054b55f1fde1
Marc Ientilucci <marc.ientilucci@gmail.com>
2020-06-15 18:05:32 -0400

    grouped artists under individual releases

--- NewReleases.py
+++ NewReleases.py
@@ -5,18 +5,16 @@ with open('albums.json') as f:
 
 # print(album_items)
 
-print("\nSINGLES/ALBUMS: ")
-
+print("\nNew Music:\n")
 album_items = (data['albums']['items'])
 for key in range(len(album_items)):
     if album_items[key]['album_type'] == 'album':
-        print('album: ' + (album_items[key]['name']))
+        print('ALBUM: ' + (album_items[key]['name']))
     elif album_items[key]['album_type'] == 'single':
-        print("single: " + (album_items[key]['name']))
-
-
-print("\nARTISTS: ")
-for key in range(len(album_items)):
+        print("SINGLE: " + (album_items[key]['name']))
     artists = (album_items[key]['artists'])
     for key in range(len(artists)):
-        print(artists[key]['name'])
+        print("by " + artists[key]['name'])
+    print()
+
+
