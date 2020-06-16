83062136d260125adc4170d3fabebd5b5b42fab6
Marc Ientilucci <marc.ientilucci@gmail.com>
2020-06-15 20:27:49 -0400

    changed artists to be unreliant on album_items

--- NewReleases.py
+++ NewReleases.py
@@ -4,15 +4,18 @@ with open('albums.json') as f:
     data = json.load(f)
 
 # print(album_items)
+# print(json.dumps(data, indent=4, sort_keys=True))
+# print(json.dumps(data['albums']['items'][key]['artists'], indent=4, sort_keys=True))
 
 print("\nNew Music:\n")
+
 album_items = (data['albums']['items'])
 for key in range(len(album_items)):
     if album_items[key]['album_type'] == 'album':
         print('ALBUM: ' + (album_items[key]['name']))
     elif album_items[key]['album_type'] == 'single':
         print("SINGLE: " + (album_items[key]['name']))
-    artists = (album_items[key]['artists'])
+    artists = data['albums']['items'][key]['artists']
     for key in range(len(artists)):
         print("by " + artists[key]['name'])
     print()
